843193f4022d5da52d7f53f4faf7a54ac474f5d1
suzannelv <lvmeijuan9011@gmail.com>
2023-01-12 23:07:21 +0100

    more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 
 @Component({
@@ -6,13 +9,56 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor(private http:HttpClient,
+    private alertC: AlertController) {
+
+  }
+  res:any;
+  ngOnInit(): void {
+  let url = 'https://sangria-walkingstick-kit.cyclic.app/api/movies'
+    this.http.get(url).subscribe((res)=>{
+      console.log(res);
+      this.res=res;
+    })
+  }
   onSearchChange(e:any){
     // console.log(e);
     let value = e.detail.value
-    console.log(value);
-    
-    
+    console.log(value); 
+  }
+
+  showAlert(){
+    // xxx.create().then(): 弹框服务，创建弹框().然后(弹框do...事)
+    this.alertC
+      .create({
+        header:"Attention",
+        // subHeader:"Important message",
+        message:"Il faut charger!",
+        buttons:[{
+          text:"ok",
+          handler:() => {
+            console.log("tout de suite")
+          },
+        },
+        {
+          text:"supprimer",
+          handler:() => {
+            console.log("plus tard")
+          },
+        }
+      ]
+      }).then((res)=>res.present())
+  // present():弹出  res是创建出来的对话框，  对话框.弹出()
+  }
+
+  loadData(e) {
+   console.log("bas")
+  //  加载更多
   }
 }
+
+
+
+
+
